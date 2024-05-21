d01af8c2487764c90cd206e217fc78e13e3f4818
Bagdat <clonn262@gmail.com>
2024-05-21 19:43:50 +0500

    fixed

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -1,12 +1,10 @@
-import NewsDetailedPage from "@/pages/NewsDetailed/ui/NewsDetailed";
+// import NewsDetailedPage from "@/pages/NewsDetailed/ui/NewsDetailed";
 import Layout from "@/widgets/Layout/Layout";
 
 export default function Home() {
   return (
     <Layout>
-      <main className="container">
-        <NewsDetailedPage />
-      </main>
+      <main className="container">{/* <NewsDetailedPage /> */}</main>
     </Layout>
   );
 }
